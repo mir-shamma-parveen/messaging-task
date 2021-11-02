918152c348cb72320ebdd2dae79c1bee1f275aa9
Shamma1996 <shamma@technoexponent.com>
2021-11-02 15:41:57 +0530

    test mail task

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Message from './Components/Message/Index'
+
+function App() {
+  return (
+    <div className="App">
+      <Message />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Message/MessageBody.js
@@ -0,0 +1,163 @@
+import React from "react";
+
+const MessageBody = () => {
+
+    return (
+        <div className="messageBody">
+            <div className="messageBodyTopSection">
+                <div className="messageBodySearch">
+                    <i className="material-icons-outlined searchIcon">
+                        search
+                    </i>
+                    <input type="text" />
+                </div>
+            </div>
+            <div className="messageListingWrap">
+                <div className="messageItem unread">
+                    <div className="messageItemBody">
+                        <p className="MessageTitle">Lorem Ipsum dolor sit amet edificing elit consetature</p>
+                        <p className="contactNo"><span className="material-icons-outlined callIcon">
+                            phone
+                        </span>+91-98300-98300</p>
+                    </div>
+                    <div className="messageDate">
+                        10-28-2021
+                        <div className="actionBtnWrap">
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    outlined_flag
+                                </span>
+                            </button>
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    report_problem
+                                </span>
+                            </button>
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    delete
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="messageItem">
+                    <div className="messageItemBody">
+                        <p className="MessageTitle">Lorem Ipsum dolor sit amet edificing elit consetature</p>
+                        <p className="contactNo"><span className="material-icons-outlined callIcon">
+                            phone
+                        </span>+91-98300-98300</p>
+                    </div>
+                    <div className="messageDate">
+                        10-28-2021
+                        <div className="actionBtnWrap">
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    outlined_flag
+                                </span>
+                            </button>
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    report_problem
+                                </span>
+                            </button>
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    delete
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="messageItem unread">
+                    <div className="messageItemBody">
+                        <p className="MessageTitle">Lorem Ipsum dolor sit amet edificing elit consetature</p>
+                        <p className="contactNo"><span className="material-icons-outlined callIcon">
+                            phone
+                        </span>+91-98300-98300</p>
+                    </div>
+                    <div className="messageDate">
+                        10-28-2021
+
+                        <div className="actionBtnWrap">
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    outlined_flag
+                                </span>
+                            </button>
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    report_problem
+                                </span>
+                            </button>
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    delete
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="messageItem">
+                    <div className="messageItemBody">
+                        <p className="MessageTitle">Lorem Ipsum dolor sit amet edificing elit consetature Lorem Ipsum dolor sit amet edificing elit consetature</p>
+                        <p className="contactNo"><span className="material-icons-outlined callIcon">
+                            phone
+                        </span>+91-98300-98300</p>
+                    </div>
+                    <div className="messageDate">
+                        10-28-2021
+                        <div className="actionBtnWrap">
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    outlined_flag
+                                </span>
+                            </button>
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    report_problem
+                                </span>
+                            </button>
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    delete
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="messageItem unread">
+                    <div className="messageItemBody">
+                        <p className="MessageTitle">Lorem Ipsum dolor sit amet edificing elit consetature Lorem Ipsum dolor sit amet edificing elit consetature</p>
+                        <p className="contactNo">
+                            <span className="material-icons-outlined callIcon">
+                                phone
+                            </span>+91-98300-98300</p>
+                    </div>
+                    <div className="messageDate">
+                        10-28-2021
+                        <div className="actionBtnWrap">
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    outlined_flag
+                                </span>
+                            </button>
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    report_problem
+                                </span>
+                            </button>
+                            <button className="roundedIconBtn">
+                                <span className="material-icons-outlined">
+                                    delete
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MessageBody;
